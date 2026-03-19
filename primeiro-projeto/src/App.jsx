849d08ad8b43d5a1ec6ba99b74da378baf81b3a3
Sofia Trindade <sofiaatrindadee02@gmail.com>
2026-03-18 21:41:55 -0300

    Componente de navegação

--- primeiro-projeto/src/App.jsx
+++ primeiro-projeto/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logoImg from './assets/imagens/logo.png'
+import Navbar from "./components/Navbar/Navbar";
 
 
 // Componente em classe é uma classe que herda a classe component do React,
@@ -9,23 +9,8 @@ class App extends React.Component {
 
   // Método responsável por renderizar o conteúdo HTML do nosso componente
   render() {
-    return (
-    <header>
-      <nav>
-        <div className="navbar">
-          <img src={logoImg} alt="" />
-          <h1>Space Flight News</h1>
-        </div>
-
-        <ul className="nav-list">
-          <li><a href="/"></a>Home</li>
-          <li><a href="/"></a>Trending</li>
-          <li><a href="/"></a>Categories</li>
-          <li><a href="/"></a>About us</li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return <Navbar />;
+    
   }
 }
 
